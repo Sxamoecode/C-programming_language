3ad06be98e7713f5cd532d275093338eba6529df
Ayandare Samuel <samuelayandare@gmail.com>
2022-08-31 13:26:41 -0700

    update

---
+++ 0x04-Chapter4-Practice_set/0x10-Check_Prime.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, i, flag=0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    //0 and 1 are not prime number
+    //Change flag to 1 for non-prime number
+    if (num == 0 || num  == 1) {
+        flag = 1;
+    }
+
+    for (i = 2; i <= num/2; ++i)
+    {
+        //if num is divisible by i, then n is not prime
+        // change flag to 1 for non-prime number
+        if (num % i == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+    //flag is 0 for prime numbers
+    if (flag == 0)
+    {
+        printf("%d is a prime number", num);
+    }
+    else
+    {
+        printf("%d is not a prime number", num);
+    }
+
+    return 0;
+}
