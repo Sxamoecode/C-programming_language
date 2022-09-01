7350f5fec21f4650952cc16a8dab62654cafefc4
Ayandare Samuel <samuelayandare@gmail.com>
2022-09-01 08:21:30 -0700

    Update

--- 0x04-Chapter4-Practice_set/0x01-Multiplication-table.c
+++ 0x04-Chapter4-Practice_set/0x01-Multiplication-table.c
@@ -7,7 +7,7 @@ int main()
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    for (int i = 10; i <= 10; i++)
+    for (int i = 1; i <= 10; i++)
     {
         printf("%d * %2d    =    %d\n", num, i, num*i);
     }
